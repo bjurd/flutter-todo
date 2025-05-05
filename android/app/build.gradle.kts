09b82e6c40712378370c0994b44579722879ff5f
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-05 12:42:56 -0400

    Enable cleartext traffic

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        manifestPlaceholders["usesCleartextTraffic"] = "true"
     }
 
     buildTypes {
