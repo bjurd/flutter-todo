d333e9b4712e5109a9a93474fda2d731306a1a09
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-05 14:40:36 -0400

    Screw naming conventions

--- lib/project.dart
+++ lib/project.dart
@@ -1,40 +1,19 @@
 class Project
 {
-  String _name = "";
-  int _numOfTasks = 0;
+  String name = "";
+  int numOfTasks = 0;
 
   Project.fromMap(Map<String, dynamic> map)
   {
-    _name = map["name"];
-    _numOfTasks = map["numOfTasks"];
+    name = map["name"];
+    numOfTasks = map["numOfTasks"];
   }
-
-  /*
-   * Getters
-   */
-  String get name => _name;
-
-  int get numOfTasks => _numOfTasks;
 
   Map toJson()
   {
     return {
-      "name": _name,
-      "numOfTasks": _numOfTasks
+      "name": name,
+      "numOfTasks": numOfTasks
     };
-  }
-
-
-  /*
-   * Setters
-   */
-  set name(String value)
-  {
-    _name = value;
-  }
-
-  set numOfTasks(int value)
-  {
-    _numOfTasks = value;
   }
 }
